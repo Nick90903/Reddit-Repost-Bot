5b8d01429715d192bfd0d42dcde483f68ba93611
Nick <nhatchew03@gmail.com>
2023-06-29 10:36:36 -0500

    Refactored config loading

--- utilities.js
+++ utilities.js
@@ -4,6 +4,7 @@ const snoowrap = require('snoowrap');
 const { TwitterApi } = require('twitter-api-v2');
 let config = '';
 const keys = require('./keys.json');
+const { rejects } = require('assert');
 let userClient;
 let r = '';
 let lastID = '';
@@ -11,9 +12,17 @@ let post = '';
 let title = '';
 let id = '';
 
+function loadConfig() {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./configuration.json', 'utf-8', (err, data) => {
+            if (err) reject(err);
+            resolve(JSON.parse(data));
+        });
+    });
+}
+
 function initSnoo() {
     console.log('Connecting to Reddit');
-    fs.readFile('./configuration.json',(err, data) => {config = JSON.parse(data)})
     r = new snoowrap({
         userAgent: keys.userAgent,
         clientId: keys.clientID,
@@ -22,45 +31,6 @@ function initSnoo() {
     });
     console.log('snoowrapper initialized');
 };
-
-async function grabNew() {
-    fs.readFile('./configuration.json',(err, data) => {config = JSON.parse(data)})
-    post = await r.getSubreddit(config.subreddit).getNew({limit: 1});
-    title = post[0].title;
-    id = post[0].id;
-    lastID = config.last;
-    saveID();
-}
-
-function saveID() {
-    if(lastID == id) {
-    } else {
-        fs.readFile('./configuration.json',(err, data) => {
-            if (err){
-                console.log(err);
-            } else {
-            obj = JSON.parse(data); //now it an object
-            obj.last = id; //add some data
-            json = JSON.stringify(obj); //convert it back to json
-            fs.writeFile('./configuration.json', json, 'utf8', (err) => {
-                if(err) throw err;
-            }); // write it back 
-        }});
-        console.log('New post detected, ID:', id, ' Title:', title, ' Posting to Twitter in', config.postDelay, ' ms');
-        setTimeout(() => updateTwitter(id), config.postDelay);
-    }
-}
-
-async function updateTwitter(ident) {
-    const tit = await r.getSubmission(ident).fetch();
-    if(tit.removed_by_category){
-        console.log('Post', tit.title, 'Removed')
-    } else {
-        //Edit the body of the tweet here.
-        const {data: createdTweet} = await userClient.v2.tweet(tit.title + 'is #Free on r/FGF \n \n #FGF #FreeGameFindings \n https://www.reddit.com/comments/' + ident)
-       console.log('Tweet', createdTweet.id, ':', createdTweet.text);
-    }
-}
 
 async function initTwitter() {
     console.log('Connecting to Twitter');
@@ -73,9 +43,41 @@ async function initTwitter() {
     console.log('Twitter Connected');
 }
 
+async function grabNew() {
+    config = await loadConfig();
+    post = await r.getSubreddit(config.subreddit).getNew({limit: 1});
+    title = post[0].title;
+    id = post[0].id;
+    lastID = config.last;
+    saveID();
+}
+
+async function saveID() {
+    if(lastID == id) return;  
+        obj = await loadConfig(); //now it an object
+        obj.last = id; //add some data
+        json = JSON.stringify(obj); //convert it back to json
+        fs.writeFile('./configuration.json', json, 'utf8', (err) => {
+            if(err) throw err;
+        }); 
+
+    console.log('New post detected, ID:', id, ' Title:', title, ' Posting to Twitter in', config.postDelay, ' ms');
+    setTimeout(() => updateTwitter(id), config.postDelay);
+}
+
+async function updateTwitter(ident) {
+    const tit = await r.getSubmission(ident).fetch();
+    if(tit.removed_by_category){
+        console.log('Post', tit.title, 'Removed')
+    } else {
+        //Edit the body of the tweet here.
+        const {data: createdTweet} = await userClient.v2.tweet(tit.title + ' is #Free on r/FGF \n \n #FGF #FreeGameFindings \n https://www.reddit.com/comments/' + ident)
+       console.log('Tweet', createdTweet.id, ':', createdTweet.text);
+    }
+}
+
 async function startBot() {
-    const data = await afs.readFile('./configuration.json', {encoding: 'utf-8'});
-    config = JSON.parse(data);
+    config = await loadConfig();
     console.log('Starting Reddit Repost Bot');
     initSnoo();
     await initTwitter();
